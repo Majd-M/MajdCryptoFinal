9500d15e27383023f3a74240f6e2665b01e0e972
Majd Mazin <majd.m.mazin@gmail.com>
2022-05-28 16:09:51 -0700

    BtcHour added and works Hour format is not correct

--- src/main/java/com/example/majdcryptofinal/CryptoControl.java
+++ src/main/java/com/example/majdcryptofinal/CryptoControl.java
@@ -40,6 +40,8 @@ public class CryptoControl implements Initializable{
 
     @FXML
     private LineChart<String, Float> hourChart;
+    @FXML
+    private NumberAxis hourPriceAxis;
 
     @FXML
     public LineChart<String, Float> minChart;
@@ -54,8 +56,13 @@ public class CryptoControl implements Initializable{
 
     public void doMinLoad(){
         preDrawMin();
-        CompletableFuture<ObservableList<BtcMinute>> future = new CompletableFuture<>();
-        future.supplyAsync(this::setupMinChart).thenAccept(this::drawMinChart);
+        CompletableFuture<ObservableList<BtcMinute>> minfuture = new CompletableFuture<>();
+        minfuture.supplyAsync(this::setupMinChart).thenAccept(this::drawMinChart);
+    }
+    public void doHourLoad(){
+        preDrawHour();
+        CompletableFuture<ObservableList<BtcHour>> hourfuture = new CompletableFuture<>();
+        hourfuture.supplyAsync(this::setupHourChart).thenAccept(this::drawHourChart);
     }
 
     public void preDrawMin(){
@@ -65,6 +72,16 @@ public class CryptoControl implements Initializable{
         priceAxis.setUpperBound(BtcMinute.maxVal+5);
         priceAxis.setTickUnit(5);
         dataAxis.setAnimated(true);
+        minChart.setAnimated(true);
+    }
+
+    public void preDrawHour(){
+        //Setting up the chart properties
+        hourPriceAxis.setAutoRanging(false);
+        hourPriceAxis.setLowerBound(BtcHour.minVal-20);
+        hourPriceAxis.setUpperBound(BtcHour.maxVal+20);
+        hourPriceAxis.setTickUnit(5);
+        hourPriceAxis.setAnimated(true);
         minChart.setAnimated(true);
     }
 
@@ -94,7 +111,44 @@ public class CryptoControl implements Initializable{
             while(firstTime<nextTime){
                 String chartTime=String.valueOf(firstTime);
                 Float value=prices[j];
-                System.out.println(String.format("%-5s %-10f",chartTime,value));
+//                System.out.println(String.format("%-5s %-10f",chartTime,value));
+                series.getData().add(new XYChart.Data<String, Float>(chartTime, value));
+                firstTime+=0.25;
+                j+=1;
+            }
+        }
+        return series;
+    }
+
+    public XYChart.Series<String, Float> setupHourChart(){
+        //XY series to be passed to the chart later
+//        System.out.println("HOUR DATA:");
+        XYChart.Series<String, Float> series = new XYChart.Series<>();
+        ObservableList<BtcHour> values=BtcHour.getHour();
+        for(BtcHour bt:values){
+            String time=bt.getTime();
+            float prices[]={bt.getOpen(), bt.getLow(), bt.getHigh(), bt.getClose()};
+
+            //Converting Unix time to Minutes for dsiplay and data plotting
+            int unixHours=parseInt(time);
+            Date date = new java.util.Date(unixHours*1000L);
+            SimpleDateFormat sdf = new java.text.SimpleDateFormat("HH");
+            sdf.setTimeZone(java.util.TimeZone.getTimeZone("UTC+0"));
+            String formattedDate = sdf.format(date);
+
+            time=formattedDate;
+            System.out.println(formattedDate);
+            int timeToDiv=parseInt(time);       //converstion of the time to a float
+            float firstTime=(float) timeToDiv;  //float casted
+            float nextTime=firstTime+1;         //Used to divide the time and plot Low & high
+            System.out.println(nextTime);
+            //Used to Plot the data on the category axis
+            int j=0;
+
+            while(firstTime<nextTime){
+                String chartTime=String.valueOf(firstTime);
+                Float value=prices[j];
+//                System.out.println(String.format("%-5s %-10f",chartTime,value));
                 series.getData().add(new XYChart.Data<String, Float>(chartTime, value));
                 firstTime+=0.25;
                 j+=1;
@@ -112,10 +166,20 @@ public class CryptoControl implements Initializable{
         });
     }
 
+    public void drawHourChart(XYChart.Series<String, Float> series){
+        Platform.runLater(()->{
+            //Set the Current time
+            String priceFormat=String.format("$%-10s",String.valueOf(BtcMinute.lastVal));
+            currPriceLabel.setText(priceFormat);
+            hourChart.getData().setAll(series);
+        });
+    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         preDrawMin();
         doMinLoad();
+        doHourLoad();
     }
 }
